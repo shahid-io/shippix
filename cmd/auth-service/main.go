02934414d2a0623c8e49dea3fb45539abd800da9
Shahid <razashahid532@gmail.com>
2025-07-28 21:46:01 +0530

    feat(auth-service): implement health check endpoint

--- cmd/auth-service/main.go
+++ cmd/auth-service/main.go
@@ -7,14 +7,24 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
+	"github.com/shahid-io/shippix/pkg/config"
 	"github.com/shahid-io/shippix/internal/auth/handlers"
 	"github.com/shahid-io/shippix/internal/auth/repository"
 	"github.com/shahid-io/shippix/internal/auth/service"
 )
 
 func main() {
+	dbConfig := config.LoadDatabaseConfig()
+	dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+        dbConfig.Host,
+        dbConfig.User,
+        dbConfig.Pass,
+        dbConfig.Name,
+        dbConfig.Port,
+    )
 	// dsn := "host=localhost user=shippix password=password dbname=shippix_db port=5432 sslmode=disable"
-	dsn := "host=postgres user=shippix password=password dbname=shippix_db port=5432 sslmode=disable"
+	// dsn := "host=postgres user=shippix password=password dbname=shippix_db port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
